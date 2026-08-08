5976e77d58c5ca0ee8cd89a8495f8bdb04a7effa
April Sanchez <aasanchez2022@gmail.com>
2026-08-08 13:19:59 -0700

    Add controlled contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,92 @@
+import { useState } from 'react';
+
 function Contact() {
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    comments: ''
+  });
+
+  function handleSubmit(event) {
+  event.preventDefault();
+
+  setFormData({
+    firstName: '',
+    lastName: '',
+    email: '',
+    comments: ''
+  });
+
+  alert('Thank you! Your message has been submitted.');
+}
+
   return (
     <main>
       <h2>Contact an Advisor</h2>
 
-      <form>
+     <form onSubmit={handleSubmit}>
+
         <label>
           First Name
-          <input type="text" />
+          <input
+            type="text"
+            value={formData.firstName}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                firstName: event.target.value
+              })
+            }
+          />
         </label>
 
         <label>
-          Last Name
-          <input type="text" />
-        </label>
+  Last Name
+  <input
+    type="text"
+    value={formData.lastName}
+    onChange={(event) =>
+      setFormData({
+        ...formData,
+        lastName: event.target.value
+      })
+    }
+  />
+</label>
 
-        <label>
-          Email
-          <input type="email" />
-        </label>
+<label>
+  Email
+  <input
+    type="email"
+    value={formData.email}
+    onChange={(event) =>
+      setFormData({
+        ...formData,
+        email: event.target.value
+      })
+    }
+  />
+</label>        
 
-        <label>
-          Comments
-          <textarea />
-        </label>
+<label>
+  Comments
+  <textarea
+    value={formData.comments}
+    onChange={(event) =>
+      setFormData({
+        ...formData,
+        comments: event.target.value
+      })
+    }
+  />
+</label>
 
         <button type="submit">
           Submit
         </button>
+
       </form>
     </main>
   );
